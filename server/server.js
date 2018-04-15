ce21d6d0890823d490657186344a3f0ebe3c02b7
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-04-15 17:46:27 +0800

    add mongoose validations

--- server/server.js
+++ server/server.js
@@ -8,30 +8,36 @@ mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 
-// ================= MODELS ===============================
+// ================= MODEL ===============================
 // blueprints for how to store the data
 const Todo = mongoose.model('Todo', {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 1,
+        // remove leading/trailing whitespace
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
         // regular timestamp
-        type: Number
+        type: Number,
+        default: null
     }
 });
 
 
 // How to make a new Mongoose instance to add to DB
-const newTodo = new Todo({
-    text: 'Drink chocolate milk'
-});
+// const newTodo = new Todo({
+//     text: 'Drink chocolate milk            '
+// });
 
-newTodo.save().then((doc) => {
-    console.log('saved todo', doc);
-}, (e) => {
-    console.log(e);
-});
+// newTodo.save().then((doc) => {
+//     console.log('saved todo', doc);
+// }, (e) => {
+//     console.log(e);
+// });
 
