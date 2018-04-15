1e828a4b2e3311189bff7d78b2db1d1c8cf86d9e
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-04-15 23:57:35 +0800

    move env to setup

--- server/server.js
+++ server/server.js
@@ -1,14 +1,4 @@
-// configure in package.json
-// Set to production by default from heroku
-const env = process.env.NODE_ENV || 'development';
-console.log('env ********** ', env);
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
+require('./config/config');
 
 const express = require('express');
 // middleware - parses HTTP request body -  e.g. JSON or text to JS
