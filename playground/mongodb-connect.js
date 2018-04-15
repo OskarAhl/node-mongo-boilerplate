dc615765fcd7cef85c141629383fbfe27e77059d
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-04-15 16:53:43 +0800

    playground for add + find mongoDB

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,5 +1,11 @@
 // mongoclient let's you connect to mongo server easily
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+// same as above - ES 6 destructuring
+// ObjectID => can create object IDs on the fly
+const { MongoClient, ObjectID } = require('mongodb');
+
+// get a timeStamp
+// console.log(new ObjectID());
 
 // 1: url for db, 2: cb
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
@@ -16,7 +22,8 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
           return console.log('error inserting: ', err);
         }
 
-        console.log(JSON.stringify(res.ops, undefined, 2));
+        // ids contain timestamp
+        console.log(res.ops[0]._id.getTimestamp());
     });
 
     db.close();
