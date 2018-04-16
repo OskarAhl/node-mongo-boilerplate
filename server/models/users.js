8e16217c364a06414b5fb55d778d47578e8b77a2
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-04-16 21:52:06 +0800

    add validator + user model

--- server/models/users.js
+++ server/models/users.js
@@ -1,12 +1,36 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 var User = mongoose.model('User', {
     email: {
         type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        //  can only be ONE of the email in the db
+        unique: true,
+        validate: {
+            // return true or false
+            validator: validator.email,
+            message: `{value} is not a valid email`
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    // array of tokens
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
 module.exports = { User };
