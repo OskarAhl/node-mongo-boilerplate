3ca6d036af971e5d60b279eca67486cfa1103b00
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-04-19 22:44:37 +0800

    setup users/login

--- server/server.js
+++ server/server.js
@@ -132,7 +132,10 @@ app.get('/users/me', authenticate, (req, res) => {
 app.post('/users/login', (req, res) => {
     const body = _.pick(req.body, ['email', 'password']);
     User.findByCredentials(body.email, body.password).then((user) => {
-        res.send(user);
+        return user.generateAuthToken().then((token) => {
+            // send token back to user
+            res.header('x-auth', token).send(user);
+        });
     }).catch((e) => {
         // error
         res.status(400).send();
