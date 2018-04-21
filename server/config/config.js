736dc2f0cdcd36dc5b69f53bf6d96e880c494f10
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-04-21 16:52:25 +0800

    hide local env variables

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,20 @@
 // Set to production by default from heroku
 const env = process.env.NODE_ENV || 'development';
 console.log('env ********** ', env);
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+
+if (env === 'development' || env === 'test') {
+    const config = require('./config.json');
+    const envConfig = config[env];
+
+    for (key in envConfig) {
+        process.env[key] = envConfig[key];
+    }
 }
+// ---> Same as:
+// if (env === 'development') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+// } else if (env === 'test') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+// }
