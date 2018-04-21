e3dac8f912aee232e01f1234464bfac68b82aeab
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-04-21 16:14:52 +0800

    add created by to todos

--- server/server.js
+++ server/server.js
@@ -19,9 +19,10 @@ const port = process.env.PORT;
 app.use(bodyParser.json());
 
 // POST todo
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
     const todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator: req.user.id
     });
 
     todo.save().then((doc) => {
@@ -32,8 +33,10 @@ app.post('/todos', (req, res) => {
 });
 
 // GET ALL
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+    Todo.find({
+        _creator: req.user.id,
+    }).then((todos) => {
         res.send({
             todos,
             message: 'add custom message here'
